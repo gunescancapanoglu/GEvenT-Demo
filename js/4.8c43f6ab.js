d5ad7c0e9a11113e9333c954ae70099d50bb3902
Gunes Can Capanoglu <16815827+gunescancapanoglu@users.noreply.github.com>
2020-04-24 18:20:47 +0300

    init

---
+++ js/4.8c43f6ab.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[4],{"713b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("q-layout",{attrs:{view:"lHh Lpr lFf"}},[t("q-page-container",[t("router-view")],1)],1)},r=[],l={name:"MainLayout"},u=l,o=t("2877"),i=Object(o["a"])(u,a,r,!1,null,null,null);n["default"]=i.exports}}]);
